a3b1122109b40730964c943a6986397e692659b6
ttolgaerdogan <96665330+ttolgaerdogan@users.noreply.github.com>
2023-02-09 10:56:17 +0300

    day 16 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -47,12 +47,49 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: (CatalogModel.items != null)
-              ? ListView.builder(
+          child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+              ? GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 16,
+                      crossAxisSpacing: 16),
+                  itemBuilder: (context, index) {
+                    final item = CatalogModel.items![index];
+                    return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                          decoration:
+                              const BoxDecoration(color: Colors.deepPurple),
+                          padding: const EdgeInsets.all(12),
+                          child: Text(
+                            item.name,
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        footer: Container(
+                          decoration: const BoxDecoration(color: Colors.black),
+                          padding: const EdgeInsets.all(12),
+                          child: Text(
+                            item.price.toString(),
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        child: Image.network(
+                          item.image,
+                        ),
+                      ),
+                    );
+                  },
                   itemCount: CatalogModel.items!.length,
-                  itemBuilder: (context, index) => ItemWidget(
-                        item: CatalogModel.items![index],
-                      ))
+                )
+              // ListView.builder(
+              //     itemCount: CatalogModel.items!.length,
+              //     itemBuilder: (context, index) => ItemWidget(
+              //           item: CatalogModel.items![index],
+              //         ))
               : const Center(child: CircularProgressIndicator())),
       drawer: const MyDrawer(),
     );
